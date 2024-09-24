9d8253b7ccadc2b98abb15506dd27e6d174ad24e
CodeWithRokib <rokibulhasan018722@gmail.com>
2024-09-24 12:25:44 +0600

    interface

---
+++ interface.php
@@ -0,0 +1,31 @@
+<?php
+
+interface Animal{
+    public function makeSound();
+}
+class Cat implements Animal{
+    public function makeSound(){
+        echo "Meow";
+    }
+}
+class Dog implements Animal{
+    public function makeSound(){
+        echo " Bark";
+    }
+}
+class Mouse implements Animal{
+    public function makeSound(){
+        echo " squeak";
+    }
+}
+
+$cat = new Cat();
+$dog = new Dog();
+$mouse = new Mouse();
+$animals = array($cat, $dog, $mouse);
+
+foreach($animals as $animal){
+    $animal->makeSound();
+}
+
+?>
